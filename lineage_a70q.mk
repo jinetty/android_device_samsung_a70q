aa92af5af4f099a983de5dedfcf7450d5d0bc99a
jinetty <64617882+jinetty@users.noreply.github.com>
2023-01-21 21:14:21 +0300

    Adapting for RiceDroid

--- lineage_a70q.mk
+++ lineage_a70q.mk
@@ -16,6 +16,25 @@ $(call inherit-product, device/samsung/a70q/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# RiceDroid flags
+RICE_MAINTAINER := Jinetty21
+RICE_DEVICE := Galaxy A70
+RICE_OFFICIAL := false
+RICE_CHIPSET := Snapdragon 675
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_HAS_UDFPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+SUSHI_BOOTANIMATION  := 1080
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_ENABLE_BLUR := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_BUILD_APERTURE_CAMERA := true
+WITH_GMS := false
+TARGET_CORE_GMS := false
+TARGET_CORE_GMS_EXTRAS := false
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_a70q
 PRODUCT_DEVICE := a70q
