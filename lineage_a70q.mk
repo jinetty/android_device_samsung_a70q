8b21a40d5906d254aeb6f7a11b777e0e11d931f8
jinetty <64617882+jinetty@users.noreply.github.com>
2023-02-01 22:50:07 +0300

    Adapting for Alphadroid

--- lineage_a70q.mk
+++ lineage_a70q.mk
@@ -16,6 +16,26 @@ $(call inherit-product, device/samsung/a70q/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+## AlphaDroid Flags ##
+# GAPPS main flag
+WITH_GAPPS := false
+# Compiler will only build GMS playstore services, its dependencies, and Gboard app.
+TARGET_CORE_GAPPS := false
+# extra flag under TARGET_CORE_GAPPS. Extra packages for core build type (velvet and photos)
+TARGET_CORE_GAPPS_EXTRAS := false
+
+# Alpha internal properties
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_MATLOG := false
+TARGET_USE_GRAPHENE_CAMERA := false
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+ALPHA_MAINTAINER := Jinetty21
+
+## AlphaDroid Flags ##
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_a70q
 PRODUCT_DEVICE := a70q
